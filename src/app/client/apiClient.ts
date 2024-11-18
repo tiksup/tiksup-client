77651e940285745bbdbfcd5bc80cd30320d490a8
jsusmachaca <falcorgd@gmail.com>
2024-11-18 18:29:03 -0500

    chore: chage api url

--- src/app/client/apiClient.ts
+++ src/app/client/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.GATEWAY_URL || 'http://localhost:3000',
+    baseURL: process.env.GATEWAY_URL || 'http://gateway:3000',
 });
 
 api.interceptors.request.use(
